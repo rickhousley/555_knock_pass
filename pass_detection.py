fc59b344a43c730502add912bf48c27db268c2a6
Rick Housley <rickyhousley+bug-r@gmail.com>
2015-12-04 16:25:03 -0500

    fixes

--- pass_detection.py
+++ pass_detection.py
@@ -4,6 +4,8 @@ import time
 password = [1,0,1,1]
 entered_password = []
 
+knock_speed = 1
+
 def main():
     # Setup GPIO as input for digital mic
     GPIO.setup("P8_14", GPIO.IN)
@@ -12,34 +14,28 @@ def main():
     while True:
         if not GPIO.input("P8_14"):
             print "Starting Password Check"
-            time.sleep(0.1) # Wait for bounces
+            time.sleep(knock_speed) # Wait for bounces
 
-        # Fill password
-        for i in range(0,4):
-            entered_password.append(poll_for_knock(1))
-            time.sleep(0.1) # Wait for bounces
+            # Fill password
+            for i in range(0,4):
+                entered_password.append(poll_for_knock(knock_speed))
+                time.sleep(0.1) # Wait for bounces
 
-        print entered_password
+            print entered_password
 
-        # Compare password to set password
-        if entered_password == password:
-            print "Password Correct!"
-        else:
-            print "nope, ignoring"
+            # Compare password to set password
+            if entered_password == password:
+                print "Password Correct!"
+                # It should unlock here
 
-        del entered_password[:]
+            del entered_password[:]
 
 def poll_for_knock(timeout):
     death_time = time.time() + timeout
     while (time.time() < death_time):
         if not GPIO.input("P8_14"):
             return True
-    print "timed out"
     return False
-
-
-
-
 
 if __name__ == "__main__":
     main()
