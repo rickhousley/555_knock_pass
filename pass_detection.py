39da6978cb5934467f56c8fa2ef768d7e051219b
Rick Housley <rickyhousley+bug-r@gmail.com>
2015-12-04 16:00:02 -0500

    fixed stupid error

--- pass_detection.py
+++ pass_detection.py
@@ -11,14 +11,14 @@ def main():
     while True:
         if not GPIO.input("P8_14"):
             print "Starting Password Check"
+            time.sleep(0.1) # Wait for bounces
             if poll_for_knock(10):
                 print "1"
 
 
 def poll_for_knock(timeout):
     death_time = time.time() + timeout
-
-    if (time.time() < death_time):
+    while (time.time() < death_time):
         if not GPIO.input("P8_14"):
             return True
     print "timed out"
