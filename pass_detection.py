cd1360e2b3c11ac2f30faa4d607e31792d0941b9
Rick Housley <rickyhousley+bug-r@gmail.com>
2015-12-04 15:48:00 -0500

    began pass logic

--- pass_detection.py
+++ pass_detection.py
@@ -1,16 +1,32 @@
 import Adafruit_BBIO.GPIO as GPIO
-import time
+from time import time
 
-password = [1,0,1,0]
+password = [1,0,1,1]
 
 def main():
+    # Setup GPIO as input
     GPIO.setup("P8_14", GPIO.IN)
 
+    # Wait for knock to start password
     while True:
-        # read digital
         if not GPIO.input("P8_14"):
-            print "Knock!"
-            time.sleep(.1)
+            print "Starting Password Check"
+            if poll_for_knock(2):
+                print "1"
+
+
+def poll_for_knock(timeout):
+    time = now()
+    timout = now() + timeout
+
+    if (now() > timeout):
+        if not GPIO.input("P8_14"):
+            return True
+    return False
+
+
+
+
 
 if __name__ == "__main__":
     main()
